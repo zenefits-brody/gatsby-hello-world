942ddf4fc38332f6da3acc5f1e2f9097607e01f5
Brody Chen <rufengc@zenefits.com>
2020-11-26 23:28:10 -0800

    Replace h1 by Header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
   return (
     <div style={{ color: `purple` }}>
       <Link to="/contact/">Contact</Link>
-      <h1>Hello Gatsby!</h1>
+      <Header headerText="Hi there! This is Brody." />
       <p>What a world.</p>
       <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
